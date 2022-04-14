96700fd3cc60f768ebd15c635b0593f3d8e3bf94
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2022-04-14 11:50:49 +0200

    Added the basic structure for the Courses Controller

---
+++ API/Controllers/CourseController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  [ApiController]
+  [Route("api/v1/courses")]
+  public class CourseController : ControllerBase
+  {
+    [HttpGet()]
+    public ActionResult ListAllCourses()
+    {
+      return Ok();
+
+    }
+    [HttpGet("{id}")]
+    public ActionResult GetCourse(int id)
+    {
+      return Ok();
+    }
+
+    [HttpPost()]
+    public ActionResult AddCourse()
+    {
+      return StatusCode(201);
+    }
+    [HttpPut("{id}")]
+    public ActionResult UpdateCourse(int id)
+    {
+      return NoContent();
+    }
+    [HttpDelete("{id}")]
+    public ActionResult DeleteCourse(int id)
+    {
+      return NoContent();
+    }
+  }
+}
